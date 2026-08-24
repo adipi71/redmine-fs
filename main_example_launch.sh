4cedb086bd8f4891d30861d1050a00c513b75eea
a.dipasquale <a.dipasquale@izs.it>
2026-08-25 00:15:49 +0200

    main_example_launch.sh

--- main_example_launch.sh
+++ main_example_launch.sh
@@ -1,2 +1,3 @@
 ./export-redmine-issues.sh sql/redmine_issues.sql sql/redmine_projects.sql
-./generate-jstree-json.sh --prune-empty-projects ./output/redmine_projects.tsv ./output/redmine_issues.tsv output/jstree-projects-issues.json
+./generate-jstree-json.sh --prune-empty-projects ./output/redmine_projects.tsv ./output/redmine_issues.tsv web/jstree-projects-issues.json
+python3 -m http.server 8000 --directory web
